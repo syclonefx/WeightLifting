7f2a12e313604741c2b60f013f11b41565355696
Chuck P <1077442+syclonefx@users.noreply.github.com>
2024-06-09 16:14:43 -0400

    Added Xcode project

---
+++ WeightLifting/WeightLiftingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightLiftingApp.swift
+//  WeightLifting
+//
+//  Created by Chuck Perdue on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeightLiftingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
